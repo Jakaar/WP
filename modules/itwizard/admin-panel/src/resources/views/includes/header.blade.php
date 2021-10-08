31e90d47884706e207ba8db553f32a09732fac27
Jakaar <54393995+Jakaar@users.noreply.github.com>
2021-10-08 15:20:08 +0800

    migration file changed

--- modules/itwizard/admin-panel/src/resources/views/includes/header.blade.php
+++ modules/itwizard/admin-panel/src/resources/views/includes/header.blade.php
@@ -355,19 +355,19 @@
                             <div class="dropdown-menu-header-inner pt-4 pb-4 bg-focus">
                                 <div class="menu-header-image opacity-1" style="background-image: url('/aPanel/images/city2.jpg');"></div>
                                 <div class="menu-header-content text-center text-white">
-                                    <h6 class="menu-header-subtitle mt-0"> Choose Language</h6>
+                                    <h6 class="menu-header-subtitle mt-0"> {{__('Choose Language')}}</h6>
                                 </div>
                             </div>
                         </div>
-                        <h6 tabindex="-1" class="dropdown-header"> Popular Languages</h6>
-                        <button type="button" tabindex="0" class="dropdown-item">
+                        <a href="/lang/en" type="button" tabindex="0" class="dropdown-item">
                             <span class="me-3 opacity-8 flag large US"></span>
                             English
-                        </button>
-                        <button type="button" tabindex="0" class="dropdown-item">
+                        </a>
+
+                        <a href="/lang/kr" type="button" tabindex="0" class="dropdown-item">
                             <span class="me-3 opacity-8 flag large KR"></span>
                             Korea
-                        </button>
+                        </a>
                     </div>
                 </div>
             </div>
