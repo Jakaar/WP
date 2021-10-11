87368dc4090d096ebf600d94c27ee712584c6fd1
Jakaar <54393995+Jakaar@users.noreply.github.com>
2021-10-11 16:01:15 +0800

    site info table added & seeder, migration created

---
+++ database/seeds/SiteInfoSeeder.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class SiteInfoSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('wpanel_site_info')->insert([
+            'company_name' => 'Your Company Name',
+            'site_name' => 'Your Site Name',
+            'fax' => 'Your fax here',
+            'company_register_number' => 'Your Company Fax',
+            'phone_number' => 'Your phone',
+            'address' => 'Your official address',
+            'email' => 'You Company email',
+            'site_copyright' => 'Copyright Here',
+            'logo' => null,
+            'terms_of_condition' => 'Terms of Condition',
+            'privacy' => 'Site Privacy Here'
+        ]);
+    }
+}
