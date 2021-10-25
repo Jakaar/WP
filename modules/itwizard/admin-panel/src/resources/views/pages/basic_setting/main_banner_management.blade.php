1e9c7ea9ffb2226d94c97eb01f6b335a876f5de6
talobruns <75569197+talobruns@users.noreply.github.com>
2021-10-25 11:23:35 +0800

    main banner management

--- modules/itwizard/admin-panel/src/resources/views/pages/basic_setting/main_banner_management.blade.php
+++ modules/itwizard/admin-panel/src/resources/views/pages/basic_setting/main_banner_management.blade.php
@@ -18,59 +18,143 @@
                     <i class="pe-7s-info icon-gradient bg-mean-fruit"></i>
                 </div>
                 <div>
-                   {{__('Main Banner')}}
+                   {{__('Main Banner Management')}}
                 </div>
             </div>
             <div class="page-title-actions">
                 <button type="button" data-bs-toggle="tooltip" title="Refresh" class="btn-shadow me-3 btn btn-info">
                     <i class="pe-7s-refresh-2"></i>
                 </button>
-                <button type="button" class="search-icon btn-shadow btn btn-success CreateModalShow" onclick="window.location.href='/cms/basic_setting/add_main_banner'">
-                <span class="btn-icon-wrapper pe-2 opacity-7">
-                <i class="pe-7s-plus"></i>
-                </span>
-                {{__('Create')}}
-                </button>
+               
+                    <button type="button" class="search-icon btn-shadow btn btn-success ModalShow">
+                    <span class="btn-icon-wrapper pe-2 opacity-7">
+                    <i class="pe-7s-plus"></i>
+                    </span>
+                    {{__('Create')}}
+                    </button>
+              
             </div>
 
         </div>
     </div>
     <div class="main-card mb-3 card">
         <div class="card-body">
-            <table style="width: 100%;" id="example"  class="table table-hover table-striped table-bordered">
+            <table id="noControlledTable1" style="width: 100%;" class="table table-hover table-striped table-bordered">
                 <thead>
-                <tr>
-                    <th>{{__('Number')}}</th>
-                    <th>{{__('Group Name')}}</th>
-                    <th>{{__('Code')}}</th>
-                    <th>{{__('Image')}}</th>
-                    <th>{{__('Form')}}</th>
-                    <th>{{__('Number Of Line-Wrapped Banners')}}</th>
-                    <th>{{__('Wheter Or Not To Use')}}</th>
-                    <th>{{__('Edit/Delete')}}</th>
-                </tr>
+                    <tr>
+                        <th>#</th>
+                        <td>{{__('Group Name')}}</td>
+                        <td>{{__('Code')}}</td>
+                        <td>{{__('Image')}}</td>
+                        <td>{{__('Form')}}</td>
+                        <td>{{__('Number Of Line-Wrapped Banners')}}</td>
+                        <td>{{__('Wheter Or Not To Use')}}</td>
+                        <td>{{__('Edit/Delete')}}</td>
+                    </tr>
                 </thead>
                 <tbody>
-
                     <tr>
                         <td>1</td>
-                        <td><a onclick="javascript:$.Nav('go','./banner_write', {bseq:2});">{{__('MainBanner')}}</a></td>
-                        <td><a onclick="javascript:$.Nav('go','./banner_write', {bseq:2});">{{__('Main')}}</a></td>
+                        <td>MainBanner</td>
+                        <td>Main</td>
                         <td>2</td>
                         <td>세로</td>
                         <td>1</td>
                         <td>사용함</td>
                         <td>
-                            <button class="btn-sm mb-2 me-2 btn-icon btn-icon-only btn-pill btn btn-outline-primary" onclick="window.location.href='/cms/basic_setting/edit_main_banner'">
-                                <i class="pe-7s-pen btn-icon-wrapper"></i>
+                            <button class="btn-outline-primary btn ModalShowEdit">
+                                {{ ('Edit') }}
                             </button>
-                            <button class="btn-sm mb-2 me-2 btn-icon btn-icon-only btn-pill btn btn-outline-danger">
-                                <i class="pe-7s-trash btn-icon-wrapper"></i>
+                            <button class="btn-outline-danger btn-link btn">
+                                {{ ('Delete') }}
                             </button>
                         </td>
                     </tr>
-
-
+                    <tr>
+                        <td>2</td>
+                        <td>MainBanner</td>
+                        <td>Main</td>
+                        <td>2</td>
+                        <td>세로</td>
+                        <td>1</td>
+                        <td>사용함</td>
+                        <td>
+                            <button class="btn-outline-primary btn ModalShowEdit">
+                                {{ ('Edit') }}
+                            </button>
+                            <button class="btn-outline-danger btn-link btn">
+                                {{ ('Delete') }}
+                            </button>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>3</td>
+                        <td>MainBanner</td>
+                        <td>Main</td>
+                        <td>2</td>
+                        <td>세로</td>
+                        <td>1</td>
+                        <td>사용함</td>
+                        <td>
+                            <button class="btn-outline-primary btn ModalShowEdit">
+                                {{ ('Edit') }}
+                            </button>
+                            <button class="btn-outline-danger btn-link btn">
+                                {{ ('Delete') }}
+                            </button>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>4</td>
+                        <td>MainBanner</td>
+                        <td>Main</td>
+                        <td>2</td>
+                        <td>세로</td>
+                        <td>1</td>
+                        <td>사용함</td>
+                        <td>
+                            <button class="btn-outline-primary btn ModalShowEdit">
+                                {{ ('Edit') }}
+                            </button>
+                            <button class="btn-outline-danger btn-link btn">
+                                {{ ('Delete') }}
+                            </button>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>5</td>
+                        <td>MainBanner</td>
+                        <td>Main</td>
+                        <td>2</td>
+                        <td>세로</td>
+                        <td>1</td>
+                        <td>사용함</td>
+                        <td>
+                            <button class="btn-outline-primary btn ModalShowEdit">
+                                {{ ('Edit') }}
+                            </button>
+                            <button class="btn-outline-danger btn-link btn">
+                                {{ ('Delete') }}
+                            </button>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>6</td>
+                        <td>MainBanner</td>
+                        <td>Main</td>
+                        <td>2</td>
+                        <td>세로</td>
+                        <td>1</td>
+                        <td>사용함</td>
+                        <td>
+                            <button class="btn-outline-primary btn ModalShowEdit">
+                                {{ ('Edit') }}
+                            </button>
+                            <button class="btn-outline-danger btn-link btn">
+                                {{ ('Delete') }}
+                            </button>
+                        </td>
+                    </tr>
                 </tbody>
             </table>
         </div>
@@ -78,4 +162,193 @@
 
 
 </section>
+@endsection
+@section('script')
+    <script>
+        $('.ModalShow').click(function (){
+            $('#AddRoleModal').modal('show')
+        })
+        $('.ModalShowEdit').click(function (){
+            $('#EditRoleModal').modal('show')
+        })
+    </script>
+@endsection
+@section('modal')
+    <div class="modal fade" id="AddRoleModal" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1"
+         aria-labelledby="staticBackdropLabel" aria-hidden="true">
+        <div class="modal-dialog modal-lg">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="staticBackdropLabel">{{__('Add Main Banner')}}</h5>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <div class="modal-body">
+                    <form action="#" id="create_new_role" class="row">
+                        <input type="hidden" id="u_id">
+                        <div class="mb-3 col-lg-6">
+                            <label class="form-label"> {{__('Code (English)')}} </label>
+                            <input type="text" class="form-control" placeholder="{{__('Title')}}" name="code" id="code">
+                        </div>
+                        <div class="mb-3 col-lg-6">
+                            <label class="form-label"> {{__('Group Name')}} </label>
+                            <input type="text" class="form-control" placeholder="{{__('Group Name')}}" name="group_name" id="group_name">
+                        </div>
+                
+                        <div class="mb-3 col-lg-6">
+                            <label for="exampleCity" class="form-label">{{__('Banner Form')}}</label>
+
+                                <div class="position-relative">
+
+                                    <div class="form-check">
+                                        <input class="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault1">
+                                        <label class="form-check-label" for="flexRadioDefault1">
+                                            {{__('Vertical')}}
+                                        </label>
+                                    </div>
+                                    <div class="form-check">
+                                        <input class="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault1">
+                                        <label class="form-check-label" for="flexRadioDefault1">
+                                            {{__('Horizontal')}}
+                                        </label>
+                                    </div>
+
+
+                                </div>
+                        </div>
+                        <div class="mb-3 col-lg-6">
+                            <label for="exampleCity" class="form-label">{{__('Whether Or Not To Use')}}</label>
+
+                                <div class="position-relative">
+
+                                    <div class="form-check">
+                                        <input class="form-check-input" type="radio" name="flexRadioDefault1" id="flexRadioDefault1">
+                                        <label class="form-check-label" for="flexRadioDefault1">
+                                            {{__('Used')}}
+                                        </label>
+                                    </div>
+                                    <div class="form-check">
+                                        <input class="form-check-input" type="radio" name="flexRadioDefault1" id="flexRadioDefault1">
+                                        <label class="form-check-label" for="flexRadioDefault1">
+                                            {{__('Not Used')}}
+                                        </label>
+                                    </div>
+
+
+                                </div>
+                        </div>
+                       
+                        <div class="mb-3 col-lg-6">
+                            <label class="form-label"> {{__('Banner Spacing')}} </label>
+                            <input type="text" class="form-control" placeholder="{{__('Banner Spacing')}}" name="banner_spacing" id="banner_spacing">
+                        </div>
+                        <div class="mb-3 col-lg-6">
+                            <label class="form-label"> {{__('Number Of Line-Wrapped Banners')}} </label>
+                            <select name="select" id="exampleSelect" class="form-select form-control">
+                                <option>1</option>
+                                <option>2</option>
+                                <option>3</option>
+                                <option>4</option>
+                                <option>5</option>
+                            </select>
+                        </div>
+        
+                    </form>
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">{{__('Close')}}</button>
+                    <button type="button" class="btn btn-success update-role">{{__('Save Changes')}}</button>
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+    {{-- Create role --}}
+    <div class="modal fade" id="EditRoleModal" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1"
+         aria-labelledby="staticBackdropLabel" aria-hidden="true">
+        <div class="modal-dialog modal-lg">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="EditRoleModal">{{__('Edit Main Banner')}}</h5>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <div class="modal-body">
+                    <form action="#" id="create_new_role" class="row">
+                        <input type="hidden" id="u_id">
+                        <div class="mb-3 col-lg-6">
+                            <label class="form-label"> {{__('Code (English)')}} </label>
+                            <input type="text" class="form-control" placeholder="{{__('Title')}}" name="code" id="code">
+                        </div>
+                        <div class="mb-3 col-lg-6">
+                            <label class="form-label"> {{__('Group Name')}} </label>
+                            <input type="text" class="form-control" placeholder="{{__('Group Name')}}" name="group_name" id="group_name">
+                        </div>
+                
+                        <div class="mb-3 col-lg-6">
+                            <label for="exampleCity" class="form-label">{{__('Banner Form')}}</label>
+
+                                <div class="position-relative">
+
+                                    <div class="form-check">
+                                        <input class="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault1">
+                                        <label class="form-check-label" for="flexRadioDefault1">
+                                            {{__('Vertical')}}
+                                        </label>
+                                    </div>
+                                    <div class="form-check">
+                                        <input class="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault1">
+                                        <label class="form-check-label" for="flexRadioDefault1">
+                                            {{__('Horizontal')}}
+                                        </label>
+                                    </div>
+
+
+                                </div>
+                        </div>
+                        <div class="mb-3 col-lg-6">
+                            <label for="exampleCity" class="form-label">{{__('Whether Or Not To Use')}}</label>
+
+                                <div class="position-relative">
+
+                                    <div class="form-check">
+                                        <input class="form-check-input" type="radio" name="flexRadioDefault1" id="flexRadioDefault1">
+                                        <label class="form-check-label" for="flexRadioDefault1">
+                                            {{__('Used')}}
+                                        </label>
+                                    </div>
+                                    <div class="form-check">
+                                        <input class="form-check-input" type="radio" name="flexRadioDefault1" id="flexRadioDefault1">
+                                        <label class="form-check-label" for="flexRadioDefault1">
+                                            {{__('Not Used')}}
+                                        </label>
+                                    </div>
+
+
+                                </div>
+                        </div>
+                       
+                        <div class="mb-3 col-lg-6">
+                            <label class="form-label"> {{__('Banner Spacing')}} </label>
+                            <input type="text" class="form-control" placeholder="{{__('Banner Spacing')}}" name="banner_spacing" id="banner_spacing">
+                        </div>
+                        <div class="mb-3 col-lg-6">
+                            <label class="form-label"> {{__('Number Of Line-Wrapped Banners')}} </label>
+                            <select name="select" id="exampleSelect" class="form-select form-control">
+                                <option>1</option>
+                                <option>2</option>
+                                <option>3</option>
+                                <option>4</option>
+                                <option>5</option>
+                            </select>
+                        </div>
+        
+                    </form>
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">{{__('Close')}}</button>
+                    <button type="button" class="btn btn-success" id="create_role">{{__('Save')}}</button>
+                </div>
+            </div>
+        </div>
+    </div>
 @endsection
