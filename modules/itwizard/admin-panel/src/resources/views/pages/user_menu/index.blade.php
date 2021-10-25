c8656020728d092c7b9bcafa42636a7b2653a9cc
Jakaar <54393995+Jakaar@users.noreply.github.com>
2021-10-25 12:46:25 +0800

    treeview

--- modules/itwizard/admin-panel/src/resources/views/pages/user_menu/index.blade.php
+++ modules/itwizard/admin-panel/src/resources/views/pages/user_menu/index.blade.php
@@ -157,7 +157,6 @@
     </style>
 @endsection
 @section('modal')
-
     <div class="modal fade" id="staticBackdrop" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1"
         aria-labelledby="staticBackdropLabel" aria-hidden="true">
         <div class="modal-dialog modal-lg">
