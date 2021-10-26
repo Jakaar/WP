66996fda3b6dea7348561340e3828a793a7b935f
zanabazard <64764014+zanabazard@users.noreply.github.com>
2021-10-26 13:09:04 +0800

    ckeditor

--- resources/admin_ui/js/customs/ckeditor.js
+++ resources/admin_ui/js/customs/ckeditor.js
@@ -1,7 +1,2 @@
 import ClassicEditor from "@ckeditor/ckeditor5-build-classic";
 window.ClassicEditor = ClassicEditor;
-
-ClassicEditor.create( document.querySelector( '#SiteInfoeditor3' ) )
-    .catch( error => {
-        console.error( error );
-    } );
