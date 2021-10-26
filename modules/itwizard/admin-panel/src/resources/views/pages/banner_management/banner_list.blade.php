c60a1cfcddefbc6319d8d8345ed0cd0b4eb496ef
talobruns <75569197+talobruns@users.noreply.github.com>
2021-10-26 16:32:55 +0800

    buttonuudaa oorchilj, datepicker, editor nemle

--- modules/itwizard/admin-panel/src/resources/views/pages/banner_management/banner_list.blade.php
+++ modules/itwizard/admin-panel/src/resources/views/pages/banner_management/banner_list.blade.php
@@ -334,7 +334,7 @@
                     </form>
                 </div>
                 <div class="modal-footer">
-                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">{{__('Close')}}</button>
+                    <button type="button" class="btn btn-outline-info" data-bs-dismiss="modal">{{__('Close')}}</button>
                     <button type="button" class="btn btn-success" id="create_role">{{__('Save')}}</button>
                 </div>
             </div>
--- modules/itwizard/admin-panel/src/resources/views/pages/preferences/menu_management.blade.php
+++ modules/itwizard/admin-panel/src/resources/views/pages/preferences/menu_management.blade.php
@@ -259,7 +259,7 @@
                     </form>
                 </div>
                 <div class="modal-footer">
-                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">{{__('Close')}}</button>
+                    <button type="button" class="btn btn-outline-info" data-bs-dismiss="modal">{{__('Close')}}</button>
                     <button type="button" class="btn btn-success update-role">{{__('Save Changes')}}</button>
                 </div>
             </div>
@@ -328,7 +328,7 @@
                     </form>
                 </div>
                 <div class="modal-footer">
-                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">{{__('Close')}}</button>
+                    <button type="button" class="btn btn-outline-info" data-bs-dismiss="modal">{{__('Close')}}</button>
                     <button type="button" class="btn btn-success" id="create_role">{{__('Save')}}</button>
                 </div>
             </div>
