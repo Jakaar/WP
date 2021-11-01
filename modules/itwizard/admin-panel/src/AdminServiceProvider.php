2dcd64d5cfdf8fa6ffa7c436db02a64f6a7c7e93
Jakaar <54393995+Jakaar@users.noreply.github.com>
2021-11-01 15:24:53 +0800

    last

--- modules/itwizard/admin-panel/src/AdminServiceProvider.php
+++ modules/itwizard/admin-panel/src/AdminServiceProvider.php
@@ -1,10 +1,8 @@
 <?php
 
 namespace Itwizard\Adminpanel;
-
 use App\Http\Middleware\Authenticate;
 use App\Http\Middleware\Localization;
-use Illuminate\Http\Request;
 use Illuminate\Routing\Router;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\ServiceProvider;
@@ -14,26 +12,17 @@ use Itwizard\Adminpanel\Console\ProcessCommand;
 class AdminServiceProvider extends ServiceProvider
 {
     public function boot(Router $router){
-        // $data['logo'] = DB::table('wpanel_site_info')
-        //     ->select('logo')
-        //     ->first();
-        // view()->share('logo', $data);
-//        dd(view()->share('logo', $data));
-//        dd($data['logo']->logo);
+
+        view()->share('data', $this->GetDatas());
+
         $this->loadRoutesFrom(__DIR__.'./routes/web.php');
         $this->loadRoutesFrom(__DIR__.'./routes/api.php');
         $this->loadViewsFrom(__DIR__.'./resources/views','Admin');
         $this->mergeConfigFrom(__DIR__.'./config/app.php','Admin');
         $this->mergeConfigFrom(__DIR__.'./config/menus.php','Menu');
-        $this->mergeConfigFrom(__DIR__.'./config/settings.php','setting');
         $this->loadMigrationsFrom(__DIR__.'./database/migrations');
         $this->loadJsonTranslationsFrom(__DIR__.'./resources/lang');
-        $this->loadViewComponentsAs('adminComponents',[
-
-        ]);
-//        $this->publishes([
-//            __DIR__.'./resources/lang' => resource_path('lang'),
-//        ]);
+        $this->loadViewComponentsAs('adminComponents',[]);
         $router->middlewareGroup('auth', array(
                 \App\Http\Middleware\EncryptCookies::class,
                 \Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse::class,
@@ -45,8 +34,6 @@ class AdminServiceProvider extends ServiceProvider
                 Localization::class,
             )
         );
-//        dd($data);
-//        return $data;
     }
     public function register()
     {
@@ -54,5 +41,13 @@ class AdminServiceProvider extends ServiceProvider
             ProcessCommand::class,
             ControllerCommand::class,
         ]);
+    }
+    public function GetDatas(): array
+    {
+        $data['langs'] = DB::table('wpanel_available_language')->get();
+        $data['logo'] = DB::table('wpanel_site_info')
+            ->select('logo')
+            ->first();
+        return $data;
     }
 }
--- modules/itwizard/admin-panel/src/resources/views/includes/header.blade.php
+++ modules/itwizard/admin-panel/src/resources/views/includes/header.blade.php
@@ -2,7 +2,7 @@
     <div class="app-header__logo">
         <div class="logo-src">
             <a href="/cms">
-                <img class="logo-src" src="{{$logo['logo']->logo ?? ''}}" alt="">
+                <img class="logo-src" src="{{$data['logo']->logo ?? ''}}" alt="">
             </a>
         </div>
         <div class="header__pane ms-auto">
