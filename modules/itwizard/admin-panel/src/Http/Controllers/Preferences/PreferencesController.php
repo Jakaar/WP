e4214ac12317e3ffa8f4cd0ec7021c184ed9953a
Daathyuu <30826254+Daathyuu@users.noreply.github.com>
2021-11-15 16:30:58 +0800

    admin menu

--- modules/itwizard/admin-panel/src/Http/Controllers/Preferences/PreferencesController.php
+++ modules/itwizard/admin-panel/src/Http/Controllers/Preferences/PreferencesController.php
@@ -1,4 +1,5 @@
 <?php
+
 namespace Itwizard\Adminpanel\Http\Controllers\Preferences;
 
 use Illuminate\Http\Request;
@@ -14,27 +15,31 @@ class PreferencesController extends Controller
     {
         $this->middleware('auth');
     }
-
-
-
     public function index()
     {
-        $data = DB::table('wpanel_settings')->orderby('order','ASC')->get()->groupBy('group');
-        $group = DB::table('wpanel_settings')->select('group')->orderby('group','ASC')->groupBy('group')->get();
+        $data = DB::table('wpanel_settings')->orderby('order', 'ASC')->get()->groupBy('group');
+        $group = DB::table('wpanel_settings')->select('group')->orderby('group', 'ASC')->groupBy('group')->get();
 
-        
-        return view('Admin::pages.preferences.index',[
+
+        return view('Admin::pages.preferences.index', [
             'model' => $data,
             'group' => $group,
         ]);
-
     }
 
-    public function logger(Request $request){
-        $logs = \App\LogActivity::whereDate('created_at','<=', now()->format('Y-m-d h:i:s'))->orderby('created_at','DESC')->get();
-     
-        return view('Admin::pages.preferences.logger',[
+    public function logger(Request $request)
+    {
+        $logs = \App\LogActivity::whereDate('created_at', '<=', now()->format('Y-m-d h:i:s'))->orderby('created_at', 'DESC')->get();
+
+        return view('Admin::pages.preferences.logger', [
             'logs' => $logs,
+        ]);
+    }
+
+    public function menu() {
+        $model = \App\AdminMenu::where('parent_id',null)->where('is_active', 1)->orderby('order','ASC')->get();
+        return view('Admin::pages.preferences.menu',[
+            'model' => $model,
         ]);
     }
 }
