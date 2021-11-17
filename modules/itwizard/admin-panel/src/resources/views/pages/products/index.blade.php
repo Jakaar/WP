9ef1e66a33b411dc6831a6ccaf369dd453789817
Javkhlantugs <dr.jawha@gmail.com>
2021-11-17 16:56:19 +0800

    Update index.blade.php

--- modules/itwizard/admin-panel/src/resources/views/pages/products/index.blade.php
+++ modules/itwizard/admin-panel/src/resources/views/pages/products/index.blade.php
@@ -5,7 +5,7 @@
     <div class="page-title-wrapper">
         <div class="page-title-heading">
             <div class="page-title-icon">
-                <i class="pe-7s-info icon-gradient bg-mean-fruit"></i>
+                <i class="pe-7s-keypad icon-gradient bg-mean-fruit"></i>
             </div>
             <div style="color: #222222;">
                 {{__('Product Manage')}}
@@ -205,14 +205,14 @@
             for(var i in files){
                 console.log(files)
             }
-                
-            
+
+
         })
     })
 </script>
 <script>
     $('.ModalShow').click(function(){
-        $('#staticBackdrop').modal('show')  
+        $('#staticBackdrop').modal('show')
     })
 </script>
 <script>
