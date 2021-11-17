732bd97ad422f71dbd5d1e26c3389b074bd4d7b0
Daathyuu <30826254+Daathyuu@users.noreply.github.com>
2021-11-17 09:12:08 +0800

    products

---
+++ modules/itwizard/admin-panel/src/Http/Controllers/Product/ProductCreateController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Itwizard\Adminpanel\Http\Controllers\Product;
+
+use Illuminate\Http\Request;
+use Illuminate\Routing\Controller;
+use Illuminate\Support\Facades\DB;
+
+
+class ProductCreateController extends Controller
+{
+    //
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+    public function index()
+    {
+        return view('Admin::pages.products.product_modal');
+    }
+
+}
