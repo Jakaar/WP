873918b24b2a3cd86894dc162d6a22920261ec9b
Daathyuu <30826254+Daathyuu@users.noreply.github.com>
2021-11-24 10:50:11 +0800

    Update index.blade.php

--- modules/itwizard/admin-panel/src/resources/views/pages/static_file/index.blade.php
+++ modules/itwizard/admin-panel/src/resources/views/pages/static_file/index.blade.php
@@ -34,69 +34,70 @@
                     class="btn-shadow me-3 btn btn-info">
                     <i class="pe-7s-refresh-2"></i>
                 </button>
-                @permission('file-create')
+
                 <button type="button" class="search-icon btn-shadow btn btn-success ModalShow">
                     <span class="btn-icon-wrapper pe-2 opacity-7">
                         <i class="pe-7s-plus"></i>
                     </span>
                     {{ __('Create') }}
                 </button>
-                @endpermission
+
             </div>
         </div>
     </div>
 
     <div class="row">
-      
-            <div class="col-md-12">
-                <div class="card mb-3 card-btm-border border-primary">
-                    <div class="card-body">
-                        <div class="card-title mb-3"> {{ __('Static File') }} </div>
-                        <table style="width: 100%;" id="new_table_1"
-                            class="table table-hover table-striped table-bordered">
-                            <thead>
+
+        <div class="col-md-12">
+            <div class="card mb-3 card-btm-border border-primary">
+                <div class="card-body">
+                    <div class="card-title mb-3"> {{ __('Static File') }} </div>
+                    <table style="width: 100%;" id="new_table_1" class="table table-hover table-striped table-bordered">
+                        <thead>
+                            <tr>
+                                <th> {{ __('Type') }} </th>
+                                <th> {{ __('File Absolute Path') }} </th>
+                                <th> {{ __('Status') }} </th>
+                                <th> {{ __('Created Date') }} </th>
+                                <th> {{ __('Updated Date') }} </th>
+                                <th>{{ __('Edit/Delete') }}</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            @foreach ($static_files as $static_file)
                                 <tr>
-                                    <th> {{ __('Type') }} </th>
-                                    <th> {{ __('File Absolute Path') }} </th>
-                                    <th> {{ __('Status') }} </th>
-                                    <th> {{ __('Created Date') }} </th>
-                                    <th> {{ __('Updated Date') }} </th>
-                                    <th>{{ __('Edit/Delete') }}</th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                @foreach ($static_files as $static_file)
-                                <tr>
-                                    <td>{{$static_file->type_name}} </td>
-                                    <td>client/static/{{$static_file->type_name}}/{{$static_file->file_absolute_path}}</td>
+                                    <td>{{ $static_file->type_name }} </td>
+                                    <td>client/static/{{ $static_file->type_name }}/{{ $static_file->file_absolute_path }}
+                                    </td>
                                     <td>
-                                        @if ($static_file->status=='2')
-                                           {{__('Used')}}
+                                        @if ($static_file->status == '2')
+                                            {{ __('Used') }}
                                         @elseif($static_file->status=='1')
-                                            {{__('Not Used')}}
+                                            {{ __('Not Used') }}
                                         @endif
                                     </td>
-                                    <td>{{$static_file->created_at}}</td>
-                                    <td>{{$static_file->updated_at}}</td>
+                                    <td>{{ $static_file->created_at }}</td>
+                                    <td>{{ $static_file->updated_at }}</td>
                                     <td>
-                                        @permission('file-edit')
-                                        <button class="btn-outline-primary btn ModalShowEdit editbtn" value="{{ $static_file->id }}">{{ __('Edit') }}</button>
-                                        @endpermission
-                                        @permission('file-delete')
+
+                                        <button class="btn-outline-primary btn ModalShowEdit editbtn"
+                                            value="{{ $static_file->id }}">{{ __('Edit') }}</button>
+
+
                                         <button class="btn-outline-danger btn-link btn DeleteFile"
                                             key="{{ $static_file->id }}">
                                             {{ __('Delete') }}
                                         </button>
-                                        @endpermission
+
                                     </td>
                                 </tr>
-                                @endforeach
-                            </tbody>
-                        </table>
-                    </div>
+                            @endforeach
+                        </tbody>
+                    </table>
                 </div>
             </div>
-       
+        </div>
+
     </div>
 @endsection
 
@@ -111,9 +112,9 @@
             $('#EditRoleModal').modal('show')
         })
         $(document).ready(function() {
-               
+
             $('#createForm').validate({
-                
+
                 errorPlacement: function(error, element) {
                     // Add the `invalid-feedback` class to the error element
                     error.addClass("invalid-feedback");
@@ -135,7 +136,7 @@
                 },
             });
             $('#updateForm').validate({
-              
+
                 errorPlacement: function(error, element) {
                     // Add the `invalid-feedback` class to the error element
                     error.addClass("invalid-feedback");
@@ -159,16 +160,16 @@
             });
             $('#add_file').on('click', function() {
                 if ($('#createForm').valid()) {
-                    
+
                     const data = new FormData();
-                    data.append('upload_file',$('#upload_file').prop('files')[0]);
-                    data.append('status',$('#status').val());
+                    data.append('upload_file', $('#upload_file').prop('files')[0]);
+                    data.append('status', $('#status').val());
                     const headers = {
                         'Content-Type': 'multipart/form-data',
                         'Content-Type': 'Application/json'
                     }
-                    
-                    Axios.post('/api/addfile',data, { 
+
+                    Axios.post('/api/addfile', data, {
                         headers: headers
                     }).then((resp) => {
                         Swal.fire(
@@ -184,23 +185,23 @@
                             icon: 'error',
                             title: err
                         });
-                    }); 
+                    });
                 }
             })
 
             $('#update_file').on('click', function() {
                 if ($('#updateForm').valid()) {
-                    
+
                     const data = new FormData();
-                    data.append('upload_file1',$('#upload_file1').prop('files')[0]);
-                    data.append('status1',$('#status1').val());
-                    data.append('file_id',$('#file_id').val());
+                    data.append('upload_file1', $('#upload_file1').prop('files')[0]);
+                    data.append('status1', $('#status1').val());
+                    data.append('file_id', $('#file_id').val());
                     const headers = {
                         'Content-Type': 'multipart/form-data',
                         'Content-Type': 'Application/json'
                     }
-                    
-                    Axios.post('/api/updatefile',data, { 
+
+                    Axios.post('/api/updatefile', data, {
                         headers: headers
                     }).then((resp) => {
                         Swal.fire(
@@ -216,7 +217,7 @@
                             icon: 'error',
                             title: err
                         });
-                    }); 
+                    });
                 }
             })
 
@@ -249,11 +250,11 @@
                 Axios.get('/api/editfile/' + file_id).then((resp) => {
                     $('#file_id').val(resp.data.id);
                     $('#status1').val(resp.data.status);
-                    $('#upload_file1').val(resp.data.file_absolute_path);               
+                    $('#upload_file1').val(resp.data.file_absolute_path);
                 });
             });
 
-          
+
 
             $('#reload_page').click(function() {
                 location.reload(true);
@@ -264,7 +265,8 @@
     </script>
 @endsection
 @section('modal')
-    <div class="modal fade" id="AddRoleModal" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+    <div class="modal fade" id="AddRoleModal" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1"
+        aria-labelledby="staticBackdropLabel" aria-hidden="true">
         <div class="modal-dialog modal-lg">
             <div class="modal-content">
                 <div class="modal-header bg-white shadow shadow-sm">
@@ -278,20 +280,22 @@
 
                             <div class="mb-3 col-lg-6">
                                 <label class="form-label"> {{ __('File') }} </label>
-                                <input type="file" class="form-control"  name="upload_file" id="upload_file" data-msg-required="{{ __('This Field is Required') }}" required>
+                                <input type="file" class="form-control" name="upload_file" id="upload_file"
+                                    data-msg-required="{{ __('This Field is Required') }}" required>
                             </div>
 
                             <div class="mb-3 col-lg-6">
                                 <label for="exampleCity" class="form-label">{{ __('Is Used') }}</label>
-                                <select name="status" id="status" class="form-select form-control" data-msg-required="{{ __('This Field is Required') }}" required>
-                                    <option value="" >{{ __('Select') }}</option>
-                                    <option value="2" >{{ __('Used') }}</option>
+                                <select name="status" id="status" class="form-select form-control"
+                                    data-msg-required="{{ __('This Field is Required') }}" required>
+                                    <option value="">{{ __('Select') }}</option>
+                                    <option value="2">{{ __('Used') }}</option>
                                     <option value="1">{{ __('Not Used') }}</option>
                                 </select>
                             </div>
 
-                        
-                    
+
+
                         </form>
                     </div>
 
@@ -318,14 +322,16 @@
                         <input type="hidden" id="file_id" name="file_id">
                         <div class="mb-3 col-lg-6">
                             <label class="form-label"> {{ __('File') }} </label>
-                            <input type="file" class="form-control"  name="upload_file1" id="upload_file1" data-msg-required="{{ __('This Field is Required') }}">
+                            <input type="file" class="form-control" name="upload_file1" id="upload_file1"
+                                data-msg-required="{{ __('This Field is Required') }}">
                         </div>
 
                         <div class="mb-3 col-lg-6">
                             <label for="exampleCity" class="form-label">{{ __('Is Used') }}</label>
-                            <select name="status1" id="status1" class="form-select form-control" data-msg-required="{{ __('This Field is Required') }}" required>
-                                <option value="" >{{ __('Select') }}</option>
-                                <option value="2" >{{ __('Used') }}</option>
+                            <select name="status1" id="status1" class="form-select form-control"
+                                data-msg-required="{{ __('This Field is Required') }}" required>
+                                <option value="">{{ __('Select') }}</option>
+                                <option value="2">{{ __('Used') }}</option>
                                 <option value="1">{{ __('Not Used') }}</option>
                             </select>
                         </div>
