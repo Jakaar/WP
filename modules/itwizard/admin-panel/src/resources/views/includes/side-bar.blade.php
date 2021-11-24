27d9ef3657249dd567c63be5b9ad544808b1743b
mekannnsama <worthlyomgxd@gmail.com>
2021-11-24 10:41:06 +0800

    logout homepage

--- modules/itwizard/admin-panel/src/resources/views/includes/side-bar.blade.php
+++ modules/itwizard/admin-panel/src/resources/views/includes/side-bar.blade.php
@@ -1,6 +1,6 @@
 <div class="app-sidebar sidebar-shadow">
     <div class="app-header__logo">
-        <div class="logo-src" style="background:url('{{ Config::get('setting.Admin_logo') ?? '/aPanel/imgs/logo.png' }}') "></div>
+        <div class="logo-src"></div>
         <div class="header__pane ms-auto">
             <div>
                 <button type="button" class="hamburger close-sidebar-btn hamburger--elastic">
@@ -32,23 +32,27 @@
     <div class="scrollbar-sidebar">
         <div class="app-sidebar__inner">
             <ul class="vertical-nav-menu">
-                @foreach(Config::get('Menu') as $menu=>$menus)
-                    @if(Request::is($menus['url'].'*') == $menus['url'])
-                        <li class="app-sidebar__heading">{{__($menus['title'])}}</li>
-                        @foreach($menus['menus'] as $mainmenu)
-                            @if($mainmenu['child'])
-                                <li class="{{Request::is($mainmenu['url'].'/*') ? 'mm-active' : null }}">
-                                    <a href="#" aria-expanded="{{Request::is($mainmenu['url'].'/*') ? 'true' : null }}">
-                                        <i class="metismenu-icon {{$mainmenu['icon']}} {{$mainmenu['colorClass'] ?? ''}}"></i>
-                                        {{__($mainmenu['name'])}}
+                @foreach (Config::get('Menu') as $menu => $menus)
+                    @if (Request::is($menus['url'] . '*') == $menus['url'])
+                        <li class="app-sidebar__heading">{{ __($menus['title']) }}</li>
+                        @foreach ($menus['menus'] as $mainmenu)
+                            @if ($mainmenu['child'])
+                                <li class="{{ Request::is($mainmenu['url'] . '/*') ? 'mm-active' : null }}">
+                                    <a href="#"
+                                        aria-expanded="{{ Request::is($mainmenu['url'] . '/*') ? 'true' : null }}">
+                                        <i
+                                            class="metismenu-icon {{ $mainmenu['icon'] }} {{ $mainmenu['colorClass'] ?? '' }}"></i>
+                                        {{ __($mainmenu['name']) }}
                                         <i class="metismenu-state-icon pe-7s-angle-down caret-left"></i>
                                     </a>
-                                    <ul class="{{Request::is($mainmenu['url']) ? 'mm-collapse mm-show' : null }}" style="">
-                                        @foreach($mainmenu['child'] as $child)
+                                    <ul class="{{ Request::is($mainmenu['url']) ? 'mm-collapse mm-show' : null }}"
+                                        style="">
+                                        @foreach ($mainmenu['child'] as $child)
                                             <li>
-                                                <a href="/{{__($child['url'])}}" class="{{Request::is($child['url']) ? 'mm-active' : null }}">
-                                                    {{--                                                    <i class="metismenu-icon"></i>--}}
-                                                    {{__($child['name'])}}
+                                                <a href="/{{ __($child['url']) }}"
+                                                    class="{{ Request::is($child['url']) ? 'mm-active' : null }}">
+                                                    {{-- <i class="metismenu-icon"></i> --}}
+                                                    {{ __($child['name']) }}
                                                 </a>
                                             </li>
                                         @endforeach
@@ -56,9 +60,11 @@
                                 </li>
                             @else
                                 <li>
-                                    <a href="/{{$mainmenu['url']}}" class="{{Request::is($mainmenu['url']) ? 'mm-active' : null }}">
-                                        <i class="metismenu-icon {{$mainmenu['icon']}} {{$mainmenu['colorClass'] ?? ''}}"></i>
-                                        {{__($mainmenu['name'])}}
+                                    <a href="/{{ $mainmenu['url'] }}"
+                                        class="{{ Request::is($mainmenu['url']) ? 'mm-active' : null }}">
+                                        <i
+                                            class="metismenu-icon {{ $mainmenu['icon'] }} {{ $mainmenu['colorClass'] ?? '' }}"></i>
+                                        {{ __($mainmenu['name']) }}
                                     </a>
                                 </li>
                             @endif
@@ -67,5 +73,47 @@
                 @endforeach
             </ul>
         </div>
+
+        <div class="app-sidebar__inner leftbottom">
+            <ul class="vertical-nav-menu ">
+                <li>
+                    <a onclick="document.getElementById('frm-logout').submit(); return false;" class="mm-active" style="cursor: pointer">
+                        <i class="metismenu-icon pe-7s-back "></i>
+                        {{ __('Logout')}}
+                    </a>
+                </li>
+                <li>
+                    <a href="/cms" class="mm-active">
+                        <i class="metismenu-icon pe-7s-home "></i>
+                        {{ __('Homepage')}}
+                    </a>
+                </li>
+            </ul>
+        </div>
+
+        <form id="frm-logout" action="{{ route('logout') }}" method="POST" style="display: none"></form>
+
     </div>
 </div>
+<script>
+
+</script>
+<style>
+    .leftbottom {
+        margin-left: 15px;
+        bottom: 0;
+        position: fixed;
+    }
+
+    .leftbottom .vertical-nav-menu li a.mm-active {
+        color: #3f6ad8 !important;
+        background: none !important;
+        font-weight: bold !important;
+    }
+
+    .leftbottom .vertical-nav-menu li {
+        margin-left: 0px;
+        width: 100%;
+    }
+
+</style>
