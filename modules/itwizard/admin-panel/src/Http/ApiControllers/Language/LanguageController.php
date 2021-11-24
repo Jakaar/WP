b03b460d331b8ab4a7413d9784b6e55e01e81adc
Buyankhishig1 <44495229+Buyankhishig1@users.noreply.github.com>
2021-11-24 10:52:25 +0800

    add LogActivity

--- modules/itwizard/admin-panel/src/Http/ApiControllers/Language/LanguageController.php
+++ modules/itwizard/admin-panel/src/Http/ApiControllers/Language/LanguageController.php
@@ -7,6 +7,7 @@ use Illuminate\Routing\Controller;
 use Illuminate\Support\Facades\DB;
 use Carbon\Carbon;
 use Illuminate\Support\Str;
+use App\Helper\LogActivity;
 
 class LanguageController extends Controller
 {
@@ -18,6 +19,7 @@ class LanguageController extends Controller
  
     public function createLanguage(Request $request)
     {
+        LogActivity::addToLog('Created Language');
             DB::table('wpanel_available_language')->insert([
                 'country'=>$request->country,
                 'country_code'=>$request->country_code,
@@ -28,17 +30,20 @@ class LanguageController extends Controller
 
     public function deleteLanguage(Request $request)
     {
+        LogActivity::addToLog('Deleted Language');
         DB::table('wpanel_available_language')->where('id', $request->delete_id)->delete();
         return response()->json(200,200);
     }
 
     public function editLanguage(Request $request)
     {
+        LogActivity::addToLog('Edited Language');
         $data = DB::table('wpanel_available_language')->where('id',$request->edit_language_id)->first();
         return response()->json(['msg' => __('success'),'data' => $data],200);
     }
 
     public function updateLanguage(Request $request){
+        LogActivity::addToLog('Updated Language');
         DB::table('wpanel_available_language')->where('id',$request->id)->update([
             'country'=>$request->country,
             'country_code'=>$request->country_code,
