bccf69b0562adf6a737c1eeb63df45b6bc5cf764
Buyankhishig1 <44495229+Buyankhishig1@users.noreply.github.com>
2021-11-29 09:20:46 +0800

    FAQ edit, update

--- modules/itwizard/admin-panel/src/Http/ApiControllers/FAQ/FAQController.php
+++ modules/itwizard/admin-panel/src/Http/ApiControllers/FAQ/FAQController.php
@@ -6,7 +6,7 @@ use Illuminate\Routing\Controller;
 use Illuminate\Support\Facades\DB;
 use App\Helper\LogActivity;
 use function GuzzleHttp\Promise\all;
-
+use Carbon\Carbon;
 class FAQController extends Controller
 {
     public function __construct()
@@ -39,4 +39,22 @@ class FAQController extends Controller
             ->update(['is_enable' => 0]);
         return response()->json(['msg', __('success')], 200);
     }
+    
+    public function edit(Request $request)
+    {
+        // dd($request->edit_id);
+        $data = DB::table('main__f_a_q')->where('id',$request->edit_id)->first();
+        return response()->json(['msg' => __('success'),'data' => $data],200);
+    }
+    public function update(Request $request){
+        // dd($request->all());
+            DB::table('main__f_a_q')->where('id',$request->id)->update([
+                'answer'=>$request->answer,
+                'question'=>$request->question,
+                'updated_at'=> \Carbon\Carbon::now(),
+            ]);
+            return back()->with('updated',__( $request->question." Has Been Updated" ));
+      
+    }
+
 }
--- modules/itwizard/admin-panel/src/resources/views/pages/manage_pages/__Page_types/FAQ.blade.php
+++ modules/itwizard/admin-panel/src/resources/views/pages/manage_pages/__Page_types/FAQ.blade.php
@@ -1,12 +1,26 @@
 <div class="main-card mb-3 card-btm-border border-primary card">
     <div class="card-body">
-        @if (session()->has('message'))
-            <div class="alert alert-success alert-dismissible fade show" role="alert">
-                <button type="button" class="btn-close" aria-label="Close"
-                        data-bs-dismiss="alert"></button>
-                {{ __(session()->get('message')) }}
-            </div>
-        @endif
+    @if (session()->has('message'))
+        <div class="alert alert-success alert-dismissible fade show" role="alert">
+            <button type="button" class="btn-close" aria-label="Close"
+                data-bs-dismiss="alert"></button>
+            {{ __(session()->get('message')) }}
+        </div>
+    @endif
+    @if (session()->has('errorA'))
+        <div class="alert alert-danger alert-dismissible fade show" role="alert">
+            <button type="button" class="btn-close" aria-label="Close"
+                data-bs-dismiss="alert"></button>
+            {{ __(session()->get('errorA')) }}
+        </div>
+    @endif
+    @if (session()->has('updated'))
+        <div class="alert alert-info alert-dismissible fade show" role="alert">
+            <button type="button" class="btn-close" aria-label="Close"
+                data-bs-dismiss="alert"></button>
+            {{ __(session()->get('updated')) }}
+        </div>
+    @endif
         <h5 class="card-title">{{__('FAQ')}}</h5>
         <div class="vertical-time-icons vertical-timeline vertical-timeline--animate vertical-timeline--one-column">
             @foreach($FAQ as $item)
@@ -28,7 +42,8 @@
                                 </div>
                                 <div class="col-2">
                                     <button class="btn btn-outline-danger btn-sm FAQDelete" key="{{$item->id}}"><i class="pe-7s-trash"></i></button>
-                                    <button class="btn btn-outline-info btn-sm"><i class="pe-7s-pen"></i></button>
+                                    <button class="btn btn-outline-info btn-sm editFAQ" data-id="{{$item->id}}" data-bs-toggle="modal" data-bs-target="#staticBackdropEdit" ><i class="pe-7s-pen"></i></button>
+                                    
                                 </div>
                             </div>
                             <div class="divider"></div>
@@ -72,6 +87,38 @@
             </div>
         </div>
     </div>
+
+
+    <div class="modal fade" id="staticBackdropEdit" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="modal-header bg-white shadow shadow-sm">
+                    <h5 class="modal-title card-title" id="staticBackdropLabel">{{__('Edit ')}}</h5>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <div class="modal-body">
+                    <div class="row">
+                        <form id="FAQForm1" method="POST" action="/api/FAQ/update" autocomplete="off">
+                        <input type="hidden" id="hiddenId">
+                            @csrf
+                            <div class="col-12">
+                                <label for="">{{__('Question')}}</label>
+                                <input required name="question"  id="question" type="text" class="form-control" placeholder="{{__('Write the Question')}}">
+                            </div>
+                            <div class="col-12 mt-2">
+                                <label for="answer">{{__('Answer')}}</label>
+                                <textarea required name="answer" id="answer1" class="form-control" rows="10"></textarea>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+                <div class="modal-footer card-btm-border card-shadow-primary border-primary">
+                    <button type="button" class="btn btn-outline-info" data-bs-dismiss="modal">{{__('Close')}}</button>
+                    <button type="button" class="btn btn-success updateFAQ">{{__('Save Changes')}}</button>
+                </div>
+            </div>
+        </div>
+    </div>
 @endsection
 @section('script')
     <script>
@@ -94,6 +141,27 @@
                     $(element).addClass("is-valid").removeClass("is-invalid");
                 },
             });
+
+            $('#FAQForm1').validate({
+                ignore: [],
+                errorPlacement: function(error, element) {
+                    // Add the `invalid-feedback` class to the error element
+                    error.addClass("invalid-feedback");
+                    if (element.prop("type") === "checkbox") {
+                        // error.insertAfter(element.next("label"));
+                    } else {
+                        // error.insertAfter(element);
+                    }
+                },
+                highlight: function(element, errorClass, validClass) {
+                    $(element).addClass("is-invalid").removeClass("is-valid");
+                },
+                unhighlight: function(element, errorClass, validClass) {
+                    $(element).addClass("is-valid").removeClass("is-invalid");
+                },
+            });
+
+
             $('.ShowModal').on('click', function (){
                 $('#FAQModal').modal('show')
             });
@@ -103,6 +171,8 @@
                    $('#FAQForm').submit();
                }
             });
+
+
             $('.FAQDelete').on('click', function (){
                Axios.post('/api/FAQ/delete/'+ $(this).attr('key')).then((resp)=>{
                    $('#FAQRemover-'+$(this).attr('key')).fadeOut();
@@ -119,6 +189,37 @@
                    })
                });
             });
+
+
+            $('.editFAQ').click(function() {
+                const edit_id = $(this).attr('data-id')
+                const data = {
+                    edit_id: edit_id
+                }
+                Axios.post('/api/FAQ/edit/', data).then((resp) => {
+                    $('#hiddenId').val(resp.data.data.id)
+                    $('#question').val(resp.data.data.question)
+                    $('#answer1').val(resp.data.data.answer)
+                }).catch((err) => {
+                    Swal.fire({
+                        icon: 'error',
+                        title: err
+                    });
+                });
+            })
+
+            $('.updateFAQ').click(function() {
+                if ($('#FAQForm1').valid()){
+                    let data = {
+                            id: $('#hiddenId').val(),
+                            question: $('#question').val(),
+                            answer: $('#answer1').val(),
+                        }
+                    Axios.post('/api/FAQ/update', data);
+                    $('#FAQForm1').submit();
+                }
+            })
+
         })
     </script>
 @endsection
