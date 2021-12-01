0643ec087ebaa1593e857e1cad5fdae23bd52333
mekannnsama <worthlyomgxd@gmail.com>
2021-12-01 21:08:53 +0800

    Ganaa siteInfo user

--- resources/views/client/includes/footer.blade.php
+++ resources/views/client/includes/footer.blade.php
@@ -1,47 +1,92 @@
 <footer class="footer">
     <div class="container">
-        <div class="row row-grid align-items-center mb-5">
-            <div class="col-lg-6">
-                <h3 class="text-primary font-weight-light mb-2">Thank you for supporting us!</h3>
-                <h4 class="mb-0 font-weight-light">Let's get in touch on any of these platforms.</h4>
-            </div>
-            <div class="col-lg-6 text-lg-center btn-wrapper">
-                <button target="_blank" href="https://twitter.com/" rel="nofollow" class="btn btn-icon-only btn-twitter rounded-circle" data-toggle="tooltip" data-original-title="Follow us">
-                    <span class="btn-inner--icon"><i class="fa fa-twitter"></i></span>
-                </button>
-                <button target="_blank" href="https://www.facebook.com//" rel="nofollow" class="btn-icon-only rounded-circle btn btn-facebook" data-toggle="tooltip" data-original-title="Like us">
-                    <span class="btn-inner--icon"><i class="fab fa-facebook"></i></span>
-                </button>
-                <button target="_blank" href="https://dribbble.com/" rel="nofollow" class="btn btn-icon-only btn-dribbble rounded-circle" data-toggle="tooltip" data-original-title="Follow us">
-                    <span class="btn-inner--icon"><i class="fa fa-twitter-square"></i></span>
-                </button>
-                <button target="_blank" href="https://github.com/" rel="nofollow" class="btn btn-icon-only btn-github rounded-circle" data-toggle="tooltip" data-original-title="Star on Github">
-                    <span class="btn-inner--icon"><i class="fa fa-github"></i></span>
-                </button>
-            </div>
-        </div>
-        <hr>
-        <div class="row align-items-center justify-content-md-between">
-            <div class="col-md-6">
-                <div class="copyright">
-                    © 2022 <a href="" target="_blank">IT Wizard</a>.
+        <div class="content">
+            <div class="row">
+                <div class="col-md-3 col-12">
+                    <div class="column">
+                        <h4 class="mt-3">Company</h4>
+                        <ul>
+                            @foreach ($data['langs'] as $key => $lang)
+                                <div class="tab-pane fade @if ($key === 0) active show @endif"
+                                    id="c_tab-animated1-{{ $lang->id }}">
+                                    <li>
+                                        {{-- <a href="javascript:;"> --}}
+                                        {{ __('Company Name') }} : {{ json_decode($site_info->company_name, true)[$lang->country_code] }}
+                                        {{-- {{ __('Company Name') }} : {{ $site_info->company_name }} --}}
+                                        {{-- </a> --}}
+                                    </li>
+                                    <li>
+                                        {{ __('Site Name') }} : {{ json_decode($site_info->site_name, true)[$lang->country_code] }}
+                                    </li>
+                                    <li class="nav-item">
+                                        {{ __('CR Number') }} : {{ $site_info->company_register_number }}
+                                    </li>
+                                    <li>
+                                        {{ __('Address') }} : {{ json_decode($site_info->address, true)[$lang->country_code] }}
+                                    </li>
+                                </div>
+                            @endforeach
+                        </ul>
+                    </div>
                 </div>
-            </div>
-            <div class="col-md-6">
-                <ul class="nav nav-footer justify-content-end">
-                    <li class="nav-item">
-                        <a href="" class="nav-link" target="_blank">Creative Tim</a>
-                    </li>
-                    <li class="nav-item">
-                        <a href="" class="nav-link" target="_blank">About Us</a>
-                    </li>
-                    <li class="nav-item">
-                        <a href="https://www.creative-tim.com/blog" class="nav-link" target="_blank">Blog</a>
-                    </li>
-                    <li class="nav-item">
-                        <a href="" class="nav-link" target="_blank">License</a>
-                    </li>
-                </ul>
+                <div class="col-md-4 col-12">
+                    <div class="column">
+                        <h4 class="mt-3">Customer</h4>
+                        <ul>
+                            @foreach ($data['langs'] as $key => $lang)
+                                <div class="tab-pane fade @if ($key === 0) active show @endif"
+                                    id="c_tab-animated1-{{ $lang->id }}">
+                                    <li class="nav-item">
+                                        {{ __('Phone Number') }} : {{ $site_info->phone_number }}
+                                    </li>
+                                    <li>
+                                        <i class="ni ni-watch-time"></i>
+                                        AM 09:30 - PM 18:30
+                                    </li>
+                                    <li class="nav-item">
+                                        {{ __('Fax Number') }} : {{ $site_info->fax }}
+                                    </li>
+                                    <li class="nav-item">
+                                        {{ __('E-mail') }} : {{ $site_info->email }}
+                                    </li>
+                                </div>
+                            @endforeach
+                        </ul>
+                    </div>
+                </div>
+                <div class="col-md-2 col-6">
+
+                </div>
+                <div class="col-md-3 col-6">
+                    <div class="column">
+                        <h4 class="mt-3">Social Feed</h4>
+                        <div class="tab-content">
+                            <ul>
+                                @foreach ($data['langs'] as $key => $lang)
+                                    <div class="tab-pane fade @if ($key === 0) active show @endif"
+                                        id="c_tab-animated1-{{ $lang->id }}">
+                                        
+                                        <li class="nav-item">
+                                            {!! json_decode($site_info->terms_of_condition, true)[$lang->country_code] !!}
+                                           {{-- {{ __('Terms of use') }} : {{ json_decode($site_info->terms_of_condition, true)[$lang->country_code] }} --}}
+                                        </li>
+                                        <li>
+                                           {{-- {{ __('Privacy Policy') }} :  --}}
+                                           {!! json_decode($site_info->privacy, true)[$lang->country_code] !!}
+                                        </li>
+                                        <li class="nav-item">
+                                            {{ __('Copyright') }} :
+                                            {{ json_decode($site_info->site_copyright, true)[$lang->country_code] }}
+                                        </li>
+                                        {{-- <li class="nav-item">
+                                            {{ __('Personal Information') }} : {{ json_decode($site_info->personal_information_manager, true)[$lang->country_code] }}
+                                        </li> --}}
+                                    </div>
+                                @endforeach
+                            </ul>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
