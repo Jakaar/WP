e28b576b5c1d157966af58662e10d915d3b55a30
Buyankhishig1 <44495229+Buyankhishig1@users.noreply.github.com>
2021-12-06 00:04:00 +0800

    hishigee commit

--- resources/views/client/includes/footer.blade.php
+++ resources/views/client/includes/footer.blade.php
@@ -65,13 +65,13 @@
                                     <div class="tab-pane fade @if ($key === 0) active show @endif"
                                         id="c_tab-animated1-{{ $lang->id }}">
                                         <li class="nav-item">
-                                            {!! json_decode($site_info->terms_of_condition_name_url, true)[session()->get('locale')]  !!}
+                                            {!! json_decode($site_info->terms_of_condition_name_url, true)[session()->get('locale')]  ?? '' !!}
                                         </li>
                                         <li>
-                                            {!! json_decode($site_info->privacy_name_url, true)[session()->get('locale')]  !!}
+                                            {!! json_decode($site_info->privacy_name_url, true)[session()->get('locale')]  ?? '' !!}
                                         </li>
                                         <li class="nav-item">
-                                            {{ __('Copyright') }} :   {!! json_decode($site_info->site_copyright, true)[session()->get('locale')]  !!}
+                                            {{ __('Copyright') }} :   {!! json_decode($site_info->site_copyright, true)[session()->get('locale')] ?? ''  !!}
                                         </li>
                                     </div>
                                 @endforeach
--- resources/views/client/layouts/master.blade.php
+++ resources/views/client/layouts/master.blade.php
@@ -13,7 +13,7 @@
     <link href="https://fonts.googleapis.com/css2?family=Rubik:ital,wght@0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
 
     @foreach($static['css'] as $css)
-        <link rel="stylesheet" href="{{$css->file_absolute_path}}">
+        <link rel="stylesheet" href="/{{$css->file_absolute_path}}">
     @endforeach
 
     <title>DEVELOPING | ++-++ </title>
