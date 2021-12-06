ead80918103c22516d0ff30827b85fec1ea5c544
Daathyuu <30826254+Daathyuu@users.noreply.github.com>
2021-12-06 14:35:16 +0800

    dropdown parent active href

--- resources/views/client/includes/header.blade.php
+++ resources/views/client/includes/header.blade.php
@@ -1,7 +1,8 @@
 @inject('t','App\Helper\Helper')
 <nav id="navbar-main" class="navbar navbar-main navbar-expand-lg bg-default navbar-dark  headroom py-2">
     <div class="container">
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbar_global" aria-controls="navbar_global" aria-expanded="false" aria-label="Toggle navigation">
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbar_global"
+            aria-controls="navbar_global" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
         <div class="navbar-collapse collapse" id="navbar_global">
@@ -9,11 +10,12 @@
                 <div class="row">
                     <div class="col-6 collapse-brand">
                         <a href="">
-                            <img src="{{asset('/client/static/img/brand/blue.png')}}" alt="">
+                            <img src="{{ asset('/client/static/img/brand/blue.png') }}" alt="">
                         </a>
                     </div>
                     <div class="col-6 collapse-close">
-                        <button type="button" class="navbar-toggler" data-toggle="collapse" data-target="#navbar_global" aria-controls="navbar_global" aria-expanded="false" aria-label="Toggle navigation">
+                        <button type="button" class="navbar-toggler" data-toggle="collapse" data-target="#navbar_global"
+                            aria-controls="navbar_global" aria-expanded="false" aria-label="Toggle navigation">
                             <span></span>
                             <span></span>
                         </button>
@@ -23,27 +25,27 @@
             <ul class="navbar-nav navbar-nav-hover align-items-lg-center ml-lg-auto">
                 <a href="/" class="nav-link">
                     <i class="ni ni-app d-lg-none"></i>
-                    <span class="nav-link-inner--text"><b>{{__('Home')}}</b></span>
+                    <span class="nav-link-inner--text"><b>{{ __('Home') }}</b></span>
                 </a>
                 <a href="/customer/products" class="nav-link">
                     <i class="ni ni-app d-lg-none"></i>
-                    <span class="nav-link-inner--text"><b>{{__('Products')}}</b></span>
+                    <span class="nav-link-inner--text"><b>{{ __('Products') }}</b></span>
                 </a>
-                @foreach($Main['menu'] as $menu)
+                @foreach ($Main['menu'] as $menu)
                     @if (count($menu->childrenCategories) > 0)
-                    <li class="nav-item dropdown">
-                        <a href="/{{$menu->board_master_id}}/{{$menu->id}}" class="nav-link" data-toggle="dropdown" role="button">
-                            <i class="ni ni-app d-lg-none"></i>
-                            <span class="nav-link-inner--text"><b>{!! $t->translateText($menu->name) !!}</b></span>
-                        </a>
-                        <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
-                            @foreach ($menu->childrenCategories as $childCategory)
-                                @include('client.includes.__nav', ['child_category' => $childCategory])
-                            @endforeach
-                        </div>
-                    </li>
+                        <li class="nav-item gg ">
+                            <a href="/{{ $menu->board_master_id }}/{{ $menu->id }}" class="nav-link">
+                                <i class="ni ni-app d-lg-none"></i>
+                                <span class="nav-link-inner--text"><b>{!! $t->translateText($menu->name) !!}</b></span>
+                            </a>
+                            <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+                                @foreach ($menu->childrenCategories as $childCategory)
+                                    @include('client.includes.__nav', ['child_category' => $childCategory])
+                                @endforeach
+                            </div>
+                        </li>
                     @else
-                        <a href="/{{$menu->board_master_id}}/{{$menu->id}}" class="nav-link">
+                        <a href="/{{ $menu->board_master_id }}/{{ $menu->id }}" class="nav-link">
                             <i class="ni ni-app d-lg-none"></i>
                             <span class="nav-link-inner--text"><b>{!! $t->translateText($menu->name) !!}</b></span>
                         </a>
@@ -52,36 +54,36 @@
                 <li class="nav-item dropdown">
                     <a href="" class="nav-link" data-toggle="dropdown" role="button">
                         <i class="ni ni-tablet-button d-lg-none"></i>
-                        <span class="nav-link-inner--text"><b>{{__('Language')}}</b></span>
+                        <span class="nav-link-inner--text"><b>{{ __('Language') }}</b></span>
                     </a>
                     <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
-                        @foreach($Main['language'] as $locale)
-                            <a class="dropdown-item" href="/lang/{{$locale->country_code}}">
-{{--                                <i class="ni ni-lock-circle-open text-muted"></i>--}}
-                                <b>{{$locale->country}}</b>
+                        @foreach ($Main['language'] as $locale)
+                            <a class="dropdown-item" href="/lang/{{ $locale->country_code }}">
+                                {{-- <i class="ni ni-lock-circle-open text-muted"></i> --}}
+                                <b>{{ $locale->country }}</b>
                             </a>
                         @endforeach
                     </div>
                 </li>
-                @if( auth()->check() )
-                <li class="nav-item dropdown">
-                    <a class="nav-link" href="#">
-                        <span class="nav-link-inner--text">
-                            <b>{{ auth()->user()->email }}</b></span>
-                    </a>
-                    <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
-<!--                        <a class="dropdown-item" href="#">-->
-<!--                            <b>Profile</b>-->
-<!--                        </a>-->
-                        <a class="dropdown-item" href="{{route('customer.logout')}}">
-                            <b>Logout</b>
+                @if (auth()->check())
+                    <li class="nav-item dropdown">
+                        <a class="nav-link" href="#">
+                            <span class="nav-link-inner--text">
+                                <b>{{ auth()->user()->email }}</b></span>
                         </a>
-                    </div>
-                </li>
+                        <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+                            <!--                        <a class="dropdown-item" href="#">-->
+                            <!--                            <b>Profile</b>-->
+                            <!--                        </a>-->
+                            <a class="dropdown-item" href="{{ route('customer.logout') }}">
+                                <b>Logout</b>
+                            </a>
+                        </div>
+                    </li>
                 @else
-                <a href="{{route('customer.login')}}" class="nav-link">
-                    <i class="ni ni-circle-08"></i>
-                    <b>Login</b></a>
+                    <a href="{{ route('customer.login') }}" class="nav-link">
+                        <i class="ni ni-circle-08"></i>
+                        <b>Login</b></a>
                 @endif
             </ul>
         </div>
--- resources/views/client/layouts/master.blade.php
+++ resources/views/client/layouts/master.blade.php
@@ -1,47 +1,98 @@
 <!doctype html>
-<html lang="{{Session::get('locale')}}">
+<html lang="{{ Session::get('locale') }}">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+        content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link href="https://fonts.googleapis.com/css2?family=Noto+Sans+KR:wght@100;300;400;500;700;900&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Noto+Sans+KR:wght@100;300;400;500;700;900&display=swap"
+        rel="stylesheet">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Rubik:ital,wght@0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
-{{--    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css">--}}
+    <link
+        href="https://fonts.googleapis.com/css2?family=Rubik:ital,wght@0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap"
+        rel="stylesheet">
+    {{-- <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css"> --}}
 
-    @foreach($static['css'] as $css)
-        <link rel="stylesheet" href="{{$css->file_absolute_path}}">
+    @foreach ($static['css'] as $css)
+        <link rel="stylesheet" href="{{ $css->file_absolute_path }}">
     @endforeach
 
     <title>DEVELOPING | ++-++ </title>
     <style>
         body {
-            -ms-overflow-style: none; /* for Internet Explorer, Edge */
-            scrollbar-width: none; /* for Firefox */
+            -ms-overflow-style: none;
+            /* for Internet Explorer, Edge */
+            scrollbar-width: none;
+            /* for Firefox */
             overflow-y: scroll;
 
         }
+
         body::-webkit-scrollbar {
-            display: none; /* for Chrome, Safari, and Opera */
+            display: none;
+            /* for Chrome, Safari, and Opera */
         }
+
     </style>
 </head>
-    <body class="sections-page">
-        @include('client.includes.header')
-            <div class="wrapper">
-                @yield('content')
-            </div>
-        @include('client.includes.footer')
+
+<body class="sections-page">
+    @include('client.includes.header')
+    <div class="wrapper">
+        @yield('content')
+    </div>
+    @include('client.includes.footer')
 
 
-        <script src="{{asset('/client/static/js/core/jquery.min.js')}}"></script>
-        <script src="{{asset('/client/static/js/core/popper.min.js')}}"></script>
-        <script src="{{asset('/client/static/js/core/bootstrap.min.js')}}"></script>
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-easing/1.3/jquery.easing.min.js"></script>
-        <script src="https://code.jquery.com/ui/1.13.0/jquery-ui.js"></script>
-        @yield('script')
-        
-    </body>
+    <script src="{{ asset('/client/static/js/core/jquery.min.js') }}"></script>
+    <script src="{{ asset('/client/static/js/core/popper.min.js') }}"></script>
+    <script src="{{ asset('/client/static/js/core/bootstrap.min.js') }}"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-easing/1.3/jquery.easing.min.js"></script>
+    <script src="https://code.jquery.com/ui/1.13.0/jquery-ui.js"></script>
+    <style>
+        .gg {
+            position: relative;
+        }
+
+        .gg:hover .dropdown-menu {
+            display: block;
+            opacity: 1 !important;
+            pointer-events: auto;
+            visibility: visible;
+            transform: translate(0);
+            animation: none;
+        }
+
+        .gg .dropdown-menu:before {
+                background: #fff;
+                box-shadow: none;
+                content: "";
+                display: block;
+                height: 16px;
+                width: 16px;
+                left: 5px;
+                position: absolute;
+                bottom: 100%;
+                transform: rotate(-45deg) translateY(1rem);
+                z-index: -5;
+                border-radius: 0.2rem;
+            
+        }
+        .gg:hover .dropdown-menu a{
+            transition: .2s;
+
+        }
+        .gg:hover .dropdown-menu  a:hover{
+            transform: scale(1.1, 1.1);
+            transition: .2s;
+            border-radius: 5px;
+        }
+
+    </style>
+    @yield('script')
+
+</body>
+
 </html>
