f9a6410d848c2738a2db2151ca8faee15db92408
zanabazard <64764014+zanabazard@users.noreply.github.com>
2021-12-07 15:20:19 +0800

    modal register

--- resources/views/client/auth/register.blade.php
+++ resources/views/client/auth/register.blade.php
@@ -120,61 +120,48 @@
             </form>
         </div>
 </section>
+<!--modal-->
+<div class='modal fade' id='myModal' tabindex='-1' role='dialog' aria-labelledby='exampleModalLabel'
+     aria-hidden='true'>
+    <div class='modal-dialog modal-lg modal-dialog-scrollable' role='document'>
+        <div class='modal-content'>
+            <div class='modal-header'>
+                <h3 class='modal-title' id='exampleModalLabel'>Supplier information</h3>
+            </div>
+            <div class='modal-body'>
+                <div class="tacbox options col-lg-12">
+                    <input class="checkbox1" id="terms" type="checkbox" class="wizard-required" value="0" onchange="checked(this, 'f_agree2')"/>
+                    <label for="checkbox"> I agree to these <a href="#">Terms and
+                            Conditions</a><span class="text-warning">(required)</span></label>
+                    <div
+                        style="height:120px; width:100%; border:1px solid #ccc;font:16px/26px Georgia, Garamond, Serif;overflow:auto;">
+                        {{$site_info->terms_of_service_login}}
+                    </div>
+                </div>
+                <div class="tacbox options mt-3 col-lg-12">
+                    <input class="checkbox2" id="privacy" type="checkbox" class="wizard-required" value="0" onchange="checked('f_agree', this)"/>
+                    <label for="checkbox"> I agree to these <a href="#">Privacy</a><span
+                            class="text-warning">(required)</span></label>
+                    <div
+                        style="height:120px;width:100%;border:1px solid #ccc;font:16px/26px Georgia, Garamond, Serif;overflow:auto;">
+                        {{$site_info->privacy_policy_login}}
+                    </div>
+                </div>
+
+                <button class='btn btn-primary btn-save-change'  type='submit'>Save changes</button>
+            </div>
+
+        </div>
+    </div>
+<!--end modal-->
 @endsection
 @section('script')
 <link rel="stylesheet" href="/client/static/css/custom.css">
-<script type="text/javascript" src="https://code.jquery.com/jquery-3.3.1.min.js"></script>
-<script src="https://cdn.jsdelivr.net/npm/smartwizard@5/dist/js/jquery.smartWizard.min.js"
-        type="text/javascript"></script>
-<link href="https://cdn.jsdelivr.net/npm/smartwizard@5/dist/css/smart_wizard_all.min.css" rel="stylesheet"
-      type="text/css"/>
 <script>
-    $(document).ready(function () {
-        $('#smartwizard').smartWizard({
-            selected: 0, // Initial selected step, 0 = first step
-            theme: 'arrows', // theme for the wizard, related css need to include for other than default theme
-            justified: true, // Nav menu justification. true/false
-            darkMode: false, // Enable/disable Dark Mode if the theme supports. true/false
-            autoAdjustHeight: true, // Automatically adjust content height
-            cycleSteps: false, // Allows to cycle the navigation of steps
-            backButtonSupport: true, // Enable the back button support
-            enableURLhash: true, // Enable selection of the step based on url hash
-            transition: {
-                animation: 'none', // Effect on navigation, none/fade/slide-horizontal/slide-vertical/slide-swing
-                speed: '400', // Transion animation speed
-                easing: '' // Transition animation easing. Not supported without a jQuery easing plugin
-            },
-            toolbarSettings: {
-                toolbarPosition: 'bottom', // none, top, bottom, both
-                toolbarButtonPosition: 'right', // left, right, center
-                showNextButton: true, // show/hide a Next button
-                showPreviousButton: true, // show/hide a Previous button
-                toolbarExtraButtons: [] // Extra buttons to show on toolbar, array of jQuery input/buttons elements
-            },
-            anchorSettings: {
-                anchorClickable: true, // Enable/Disable anchor navigation
-                enableAllAnchors: false, // Activates all anchors clickable all times
-                markDoneStep: true, // Add done state on navigation
-                markAllPreviousStepsAsDone: true, // When a step selected by url hash, all previous steps are marked done
-                removeDoneStepOnNavigateBack: false, // While navigate back done step after active step will be cleared
-                enableAnchorOnDoneStep: true // Enable/Disable the done steps navigation
-            },
-            keyboardSettings: {
-                keyNavigation: true, // Enable/Disable keyboard navigation(left and right keys are used if enabled)
-                keyLeft: [37], // Left key code
-                keyRight: [39] // Right key code
-            },
-            lang: { // Language variables for button
-                next: 'Next',
-                previous: 'Previous'
-            },
-            disabledSteps: [], // Array Steps disabled
-            errorSteps: [], // Highlight step with errors
-            hiddenSteps: [] // Hidden steps
-        });
+    $(document).ready(function(){
+        $('#myModal').modal('toggle');
     });
-</script>
-<script>
+
     jQuery(document).ready(function () {
         //Password
 
