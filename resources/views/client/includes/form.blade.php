9ac3e8d54eec878e95eb264790f2b49b8a0ab672
Daathyuu <30826254+Daathyuu@users.noreply.github.com>
2021-12-08 08:59:46 +0800

    bug fixed

--- resources/views/client/includes/form.blade.php
+++ resources/views/client/includes/form.blade.php
@@ -1,18 +1,21 @@
-
-<div class="container " >
-    <div class="row " >
+<div class="container ">
+    <div class="row ">
         <div class="divider"></div>
-        <div class="col-12">
+        <div class="col-12 mb-5">
             <div class="">
-                @if(Session::has('success'))
-                <p class="alert alert-success">{{ Session::get('success') }}</p>
+                @if (Session::has('success'))
+                    <p class="alert alert-success">{{ Session::get('success') }}</p>
                 @endif
 
-                <form  method="post" action="/user_data_send" enctype="multipart/form-data">
+                <form method="post" action="/user_data_send" enctype="multipart/form-data">
                     @csrf
-                    <input type="hidden" name="form_id" value="{{$datas['form_builded']->id}}">
+                    <div class="mb-3">
+                        <label for="" class="form-label"> {{ __('Email') }} </label>
+                        <input type="email" class="form-control" required name="_email" placeholder="Enter a your email">
+                    </div>
+                    <input type="hidden" name="form_id" value="{{ $datas['form_builded']->id }}">
                     <div id="fb-editor"></div>
-                    <button type="submit" class="btn btn-primary">{{__('submit')}}</button>
+                    <button type="submit" class="btn btn-primary float-right">{{ __('submit') }}</button>
                 </form>
             </div>
         </div>
@@ -22,17 +25,16 @@
 </div>
 
 @section('script')
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/jqueryui/1.11.4/jquery-ui.min.js"></script>
-  <script src="https://formbuilder.online/assets/js/form-builder.min.js"></script>
-  <script src="https://formbuilder.online/assets/js/form-render.min.js"></script>
-<script>
-    $(document).ready(function (){
-        const receivedData = '{!!$datas['form_builded']->data!!}'
-        var formRenderOptions = {
-            formData: receivedData,
-        }
-        $('#fb-editor').formRender(formRenderOptions)
-    });
-
-</script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jqueryui/1.11.4/jquery-ui.min.js"></script>
+    <script src="https://formbuilder.online/assets/js/form-builder.min.js"></script>
+    <script src="https://formbuilder.online/assets/js/form-render.min.js"></script>
+    <script>
+        $(document).ready(function() {
+            const receivedData = '{!! $datas['form_builded']->data !!}'
+            var formRenderOptions = {
+                formData: receivedData,
+            }
+            $('#fb-editor').formRender(formRenderOptions)
+        });
+    </script>
 @endsection
