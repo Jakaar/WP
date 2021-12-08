3757197246bf644e1453c4110181636dc737dbbc
Javkhlantugs <dr.jawha@gmail.com>
2021-12-08 16:07:59 +0800

    something changes

--- resources/views/client/includes/header.blade.php
+++ resources/views/client/includes/header.blade.php
@@ -1,6 +1,6 @@
 @inject('t','App\Helper\Helper')
 <nav id="navbar-main" class="navbar navbar-main navbar-expand-lg bg-default navbar-dark  headroom py-2">
-    <div class="container">
+    <div class="container-fluid">
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbar_global"
             aria-controls="navbar_global" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
